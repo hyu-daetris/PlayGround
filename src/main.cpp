5c8147e45166552909882405d87a288cf8d3c09c
revsic <revsic99@gmail.com>
2019-05-29 02:07:04 +0900

    main: Add port input procedure

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1920*2, 1080, OF_FULLSCREEN);			// <-------- setup the GL context
+	ofSetupOpenGL(1920 * 2, 1080, OF_FULLSCREEN);			// <-------- setup the GL context
+
+	std::string port_name;
+	std::cout << "[*] port name : ";
+	std::cin >> port_name;
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp(new ofApp());
+	ofRunApp(new ofApp(port_name));
 
 }
